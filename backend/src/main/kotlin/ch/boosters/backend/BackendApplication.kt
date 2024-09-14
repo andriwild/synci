c29c74d94ec2894a7c7379e08247dadfa94074c8
Andri Wild <andri.wild@students.fhnw.ch>
2024-09-14 10:02:48 +0200

    add backend

---
+++ backend/src/main/kotlin/ch/boosters/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package ch.boosters.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
