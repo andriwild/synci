f8a9e61a5265d5c942562d98a2f7bd9838296782
Joel Finger <joel.finger@semabit.ch>
2024-09-15 13:13:28 +0200

    refactor code in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserConfigForm } from './components/UserConfigForm.tsx';
 import './App.css';
-import {Home} from "./pages/Home.tsx";
-import {Header} from "./components/Header.tsx";
-import {Layout} from "antd";
-import {Footer} from "./components/Footer.tsx";
+import { Home } from "./pages/Home.tsx";
+import { Header } from "./components/Header.tsx";
+import { Layout } from "antd";
+import { Footer } from "./components/Footer.tsx";
 
 function App() {
     return (
-        <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
-            <Header />
-            <Router>
+        <Router>
+            <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/config" element={<UserConfigForm />} />
                 </Routes>
-            </Router>
-            <Footer />
-        </Layout>
+                <Footer />
+            </Layout>
+        </Router>
     );
 }
 
