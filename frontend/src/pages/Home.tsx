bf30b83b361745c94e656b00beb72c11293368f7
Joel Finger <joel.finger@semabit.ch>
2024-09-15 13:47:50 +0200

    change add button and make card clickable to route to edit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import {Button, Card, Flex, List, Typography} from "antd";
+import { Plus } from "@phosphor-icons/react";
 
 export const Home = () => {
     const navigate = useNavigate();
@@ -47,13 +48,14 @@ export const Home = () => {
         <Flex vertical style={{height: '100%', width: '100%', padding: 24}}>
             <Typography.Title>Synci</Typography.Title>
             <Typography.Text>Willkommen bei Synci...</Typography.Text>
+            <Button onClick={() => navigate('/config')} style={{width: 40, height: 40, marginTop: 48, backgroundColor: '#1677ff', color: 'white'}} icon={<Plus size={'1rem'} />} />
             <List
-                style={{marginTop: 48}}
+                style={{marginTop: 6}}
                 grid={{ gutter: 16}}
                 dataSource={dataSource}
                 renderItem={(item) => (
                     <List.Item>
-                        <Card title={item.label} style={{width: 400}} onClick={() => navigate(`edit/${item.id}`)}>
+                        <Card title={item.label} style={{width: 400, cursor: 'pointer'}} onClick={() => navigate(`edit/${item.id}`)}>
                             <Flex gap={12} vertical>
 
                             <Flex vertical>
@@ -65,14 +67,12 @@ export const Home = () => {
                             <Flex vertical>
                             <Typography.Text strong>Url</Typography.Text>
                                 <Typography.Paragraph copyable>{item.url}</Typography.Paragraph>
-
                             </Flex>
                             </Flex>
                         </Card>
                     </List.Item>
                 )}
             />
-            <Button onClick={() => navigate('/config')} style={{width: 200}}>Config erstellen</Button>
         </Flex>
     );
 };
