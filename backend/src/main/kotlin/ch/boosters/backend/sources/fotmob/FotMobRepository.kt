a8a20bfec4fc2d17767fb3e1c5e769cbf83f362d
Tobias Wyss <towyss@gmail.com>
2024-09-15 21:44:46 +0200

    store teams_to_events

--- backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobRepository.kt
+++ backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobRepository.kt
@@ -8,6 +8,7 @@ import ch.boosters.data.Tables.EVENTS_TEAMS_TABLE
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 class FotMobRepository(private val dsl: DSLContext) {
@@ -26,18 +27,49 @@ class FotMobRepository(private val dsl: DSLContext) {
     fun storeEvents(events: List<FotMobEvent>): MutableList<String> {
         dsl.truncate(EVENTS_TABLE).cascade().execute()
         dsl.truncate(EVENTS_TEAMS_TABLE).cascade().execute()
-        dsl.transaction { config ->
-            events.map {
-                val transaction = DSL.using(config)
-                transaction.newRecord(EVENTS_TABLE).apply {
-                    id = it.id.toString()
-                    sourceId = 1
-                    name = it.name
-                    startsOn = it.startsOn
-                    endsOn = it.endsOn
-                }.store()
+//        dsl.transaction { config ->
+            events.forEach {
+                val transaction = dsl
 
-            }
+                val eventRecord = dsl.newRecord(EVENTS_TABLE)
+                eventRecord.setId(it.id.toString())
+                eventRecord.setSourceId(1)
+                eventRecord.setName(it.name)
+                eventRecord.setStartsOn(it.startsOn)
+                eventRecord.setEndsOn(it.endsOn)
+                eventRecord.store()
+
+                val eventTeamRecord = dsl.newRecord(EVENTS_TEAMS_TABLE);
+                eventTeamRecord.setId(UUID.randomUUID());
+                eventTeamRecord.setEventId(it.id.toString())
+                eventTeamRecord.setSourceEventId(1);
+                eventTeamRecord.setTeamId(it.homeId);
+                eventTeamRecord.setSourceTeamId(1);
+                eventTeamRecord.store()
+
+                val eventTeamRecord2 = dsl.newRecord(EVENTS_TEAMS_TABLE);
+                eventTeamRecord2.setId(UUID.randomUUID());
+                eventTeamRecord2.setEventId(it.id.toString())
+                eventTeamRecord2.setSourceEventId(1);
+                eventTeamRecord2.setTeamId(it.awayId);
+                eventTeamRecord2.setSourceTeamId(1);
+                eventTeamRecord2.store()
+            //                transaction.insertInto(EVENTS_TABLE).values(it.id.toString(), 1, it.name, it.startsOn, it.endsOn)
+
+//                transaction.insertInto(EVENTS_TEAMS_TABLE).values(UUID.randomUUID(), it.id.toString(), 1, it.homeId, 1 )
+//                transaction.insertInto(EVENTS_TEAMS_TABLE).values(UUID.randomUUID(), it.id.toString(), 1, it.awayId, 1 )
+//                transaction.newRecord(EVENTS_TABLE).apply {
+////                    id = it.id.toString()
+////                    sourceId = 1
+////                    name = it.name
+////                    startsOn = it.startsOn
+////                    endsOn = it.endsOn
+//                }.store()
+//
+//                transaction.newRecord(EVENTS_TEAMS_TABLE).apply {
+//                    it.id.toString(), 1, it.homeId, 1, }
+//
+//            }
         }
         return dsl.select(EVENTS_TABLE.ID).from(EVENTS_TABLE).fetchInto(String::class.java)
     }
