3b76f2486b0e6656f97eaecfb86ae575e7460793
Joel Finger <joel.finger@semabit.ch>
2024-09-15 14:28:55 +0200

    add route for /id

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,21 @@ import { Home } from "./pages/Home.tsx";
 import { Header } from "./components/Header.tsx";
 import { Layout } from "antd";
 import { Footer } from "./components/Footer.tsx";
+import {Content} from "antd/es/layout/layout";
+import {UserConfig} from "./pages/UserConfigs.tsx";
 
 function App() {
     return (
         <Router>
             <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
                 <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/config" element={<UserConfigForm />} />
-                </Routes>
+                <Content>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/config/" element={<UserConfig />} />
+                        <Route path="/config/:id" element={<UserConfigForm />} />
+                    </Routes>
+                </Content>
                 <Footer />
             </Layout>
         </Router>
