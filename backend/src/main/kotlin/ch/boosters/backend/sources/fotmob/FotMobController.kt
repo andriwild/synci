d3c58d2cd8a04a5bfa3482ed79532fd2cbf18a7d
Tobias Wyss <towyss@gmail.com>
2024-09-15 09:53:27 +0200

    add fotmob service to sources package

--- backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobController.kt
+++ backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobController.kt
@@ -1,9 +1,8 @@
-package ch.boosters.backend.fotmob
+package ch.boosters.backend.sources.fotmob
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
