5fba2a936ada79474debdd29075903115615facf
Joel Finger <joel.finger@semabit.ch>
2024-09-15 11:15:49 +0200

    add layout to app.tsx-file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { UserConfigForm } from './pages/UserConfigForm.tsx';
+import { UserConfigForm } from './components/UserConfigForm.tsx';
 import './App.css';
 import {Home} from "./pages/Home.tsx";
+import {Header} from "./components/Header.tsx";
+import {Layout} from "antd";
+import {Footer} from "./components/Footer.tsx";
 
 function App() {
     return (
-        <Router>
-            <Routes>
+        <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
+            <Header />
+            <Router>
+                <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/config" element={<UserConfigForm />} />
-            </Routes>
-        </Router>
+                </Routes>
+            </Router>
+            <Footer />
+        </Layout>
     );
 }
 
