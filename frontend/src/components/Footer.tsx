47eb08d05ef098cd97a564e1174fa5a9af0515b5
Joel Finger <joel.finger@semabit.ch>
2024-09-15 11:14:30 +0200

    add default header-component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import {Flex} from "antd";
+
+export const Footer = () => {
+
+    return (
+        <Flex style={{background: 'lightgrey', height: 100}}>
+            Footer
+        </Flex>
+    )
+}
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import {Flex} from "antd";
+
+export const Header = () => {
+
+    return (
+        <Flex style={{background: 'lightgrey', height: 100}}>
+            Header
+        </Flex>
+    )
+}
