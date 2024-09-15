1c46df9010d74d791e67e2b95002333197e91a9f
Joel Finger <joel.finger@semabit.ch>
2024-09-15 13:48:03 +0200

    finish basic footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,11 @@
-import {Flex} from "antd";
+import {Flex, Typography} from "antd";
 
 export const Footer = () => {
 
     return (
-        <Flex style={{background: 'lightgrey', height: 100}}>
-            Footer
+        <Flex justify={'center'} align={'center'} gap={'middle'} style={{background: '#001529', height: 100}}>
+            <Typography.Text style={{color: 'white'}}>Datenschutz</Typography.Text>
+            <Typography.Text style={{color: 'white'}}>FAQ</Typography.Text>
         </Flex>
     )
 }
