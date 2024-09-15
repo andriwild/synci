d3aebb878874417b773ac5e44a3788d314318c07
Joel Finger <joel.finger@semabit.ch>
2024-09-15 14:28:09 +0200

    only leave form in user-config-form-component

--- frontend/src/components/UserConfigForm.tsx
+++ frontend/src/components/UserConfigForm.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, Flex, Form, Select } from "antd";
-import {useNavigate} from "react-router-dom";
+import {Button, Card, Flex, Form, List, Select, Typography} from "antd";
+import {useNavigate, useParams} from "react-router-dom";
+import {Copy, Plus} from "@phosphor-icons/react";
 
 interface Team {
     id: string;
@@ -10,6 +11,46 @@ export type UserConfigValues = Team;
 export const UserConfigForm = () => {
     const [form] = Form.useForm<UserConfigValues>();
     const navigate = useNavigate();
+    const {id} = useParams();
+
+    const dataSource = [
+        {
+            id: '1',
+            label: 'SuperLeague Spiele',
+            teams: ['YoungBoys, Basel, Zürich, Lausanne'],
+            url: 'https://google.ch',
+        },
+        {
+            id: '2',
+            label: 'Manchester Konfig',
+            teams: ['Manchester United'],
+            url: 'https://google.ch',
+        },
+        {
+            id: '3',
+            label: 'Manchester Konfig',
+            teams: ['Manchester United'],
+            url: 'https://google.ch',
+        },
+        {
+            id: '4',
+            label: 'Manchester Konfig',
+            teams: ['Manchester United'],
+            url: 'https://google.ch',
+        },
+        {
+            id: '5',
+            label: 'Manchester Konfig',
+            teams: ['Manchester United'],
+            url: 'https://google.ch',
+        },
+        {
+            id: '6',
+            label: 'Manchester Konfig',
+            teams: ['Manchester United'],
+            url: 'https://google.ch',
+        },
+    ];
 
     const teams: Team[] = [
         { id: 'teamA', name: 'Team A' },
@@ -27,7 +68,7 @@ export const UserConfigForm = () => {
     };
 
     return (
-        <Flex vertical style={{height: '100%', width: '100%', padding: 24, justifyContent: "center", alignItems: 'center'}}>
+        <Flex vertical style={{height: '100%', width: '100%', padding: 24}}>
         <Card style={{ width: 600, textAlign: 'left' }}>
             <h2 style={{ marginBottom: '20px' }}>Benutzerkonfigurationen</h2>
             <p>Füge eine Konfiguration hinzu. Wähle dabei aus, für welches Team, welchen Sportler, welchen Ort oder
@@ -58,6 +99,31 @@ export const UserConfigForm = () => {
                     </Button>
                 </Flex>
             </Form>
+            <Button onClick={() => navigate('/config')} style={{width: 40, height: 40, marginTop: 48, backgroundColor: '#1677ff', color: 'white'}} icon={<Plus size={'1rem'} />} />
+            <List
+                style={{marginTop: 6}}
+                grid={{ gutter: 16}}
+                dataSource={dataSource}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card title={item.label} style={{width: 400, cursor: 'pointer'}} onClick={() => navigate(`config/${item.id}`)}>
+                            <Flex gap={12} vertical>
+
+                                <Flex vertical>
+                                    <Typography.Text strong>Mannschaften</Typography.Text>
+                                    {item.teams.map((team) => {
+                                        return <Typography.Text key={team}>{team}</Typography.Text>
+                                    })}
+                                </Flex>
+                                <Flex vertical>
+                                    <Typography.Text strong>Url</Typography.Text>
+                                    <Typography.Paragraph>{item.url}</Typography.Paragraph>
+                                </Flex>
+                            </Flex>
+                        </Card>
+                    </List.Item>
+                )}
+            />
         </Card>
         </Flex>
     );
