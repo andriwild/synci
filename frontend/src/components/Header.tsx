f9dcd6377c823eb7f335bb40c8127a0d1a08549d
Joel Finger <joel.finger@semabit.ch>
2024-09-15 12:24:14 +0200

    add header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,28 @@
-import {Flex} from "antd";
+import {Menu, Layout} from "antd";
 
 export const Header = () => {
 
+    const items = [
+        {
+            key: '1',
+            label: 'Home'
+        },
+        {
+            key: '2',
+            label: 'Konfigurationen'
+        }
+    ]
+
     return (
-        <Flex style={{background: 'lightgrey', height: 100}}>
-            Header
-        </Flex>
+        <Layout.Header>
+            <div className="demo-logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                items={items}
+                style={{ flex: 1, minWidth: 0 }}
+            />
+        </Layout.Header>
     )
 }
