aea2b4633af11ed483ea36ff4ec639d73ce9fe39
Andri Wild <andri.wild@students.fhnw.ch>
2024-09-15 12:22:55 +0200

    remove waiting on db in create_tables.sh

--- create_tables.sh
+++ create_tables.sh
@@ -6,13 +6,6 @@ DB_USER="postgres"
 DB_PASSWORD="postgres"
 DB_NAME="synci-db"
 
-echo "Wait on db-connection..."
-until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c '\q'; do
-  >&2 echo "Postgres ist noch nicht bereit - warte 5 Sekunden..."
-  sleep 5
-done
-echo "Postgres is ready!"
-
 echo "Create tables..."
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ./database/synci-schema.sql
 
