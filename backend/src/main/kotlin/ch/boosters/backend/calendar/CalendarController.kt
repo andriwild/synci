daa2169854afcaed23fcf4cd6567d1c13b913204
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-02 17:58:39 +0200

    update calendar endpoint url

--- backend/src/main/kotlin/ch/boosters/backend/calendar/CalendarController.kt
+++ backend/src/main/kotlin/ch/boosters/backend/calendar/CalendarController.kt
@@ -2,20 +2,20 @@ package ch.boosters.backend.calendar
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/calendar")
 class CalendarController(private val calendarService: CalendarService) {
 
-    @GetMapping("/create-calendar", produces = ["text/calendar"])
+    @GetMapping("/subscribe", produces = ["text/calendar"])
     fun createCalendar(): String {
         return calendarService.createCalendar()
     }
 
-    @GetMapping("/create-calendar/{teamId}", produces = ["text/calendar"])
+    @GetMapping("/subscribe/{teamId}", produces = ["text/calendar"])
     fun createCalendarFromTeam(@PathVariable teamId: String): String {
-        val r = calendarService.createCalendar(teamId)
-        println(r)
-        return r
+        return calendarService.createCalendar(teamId)
     }
 }
