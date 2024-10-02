9aa5f1f29527586bfb9d9b52f9b010c6895674d7
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-02 15:59:08 +0200

    add fotmob config endpoint

--- backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobController.kt
+++ backend/src/main/kotlin/ch/boosters/backend/sources/fotmob/FotMobController.kt
@@ -1,21 +1,32 @@
 package ch.boosters.backend.sources.fotmob
 
+import ch.boosters.backend.sources.SourceConfig
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
-
 @RestController
 @RequestMapping("/fotmob")
-class FotMobController (val fotMobService: FotMobService) {
+class FotMobController (val fotMobService: FotMobService, val config: SourceConfig) {
 
     @GetMapping("/{leagueId}")
-    fun getSuperLeagueEvents(@PathVariable leagueId: String): Mono<IntArray> {
+    fun getLeagueEvents(@PathVariable leagueId: String): Mono<IntArray> {
         val leagueOverview = fotMobService.fetchLeagueOverview(leagueId)
-        println(leagueOverview)
         return leagueOverview
     }
 
+    @GetMapping("/config")
+    fun getConfig(): Map<String, Any?> {
+        val config = config.fotmob
+        val leagues = config.leagues.map { league ->
+            mapOf("id" to league.id, "name" to league.name)
+        }
+        return mapOf(
+            "name" to config.name,
+            "url" to config.url,
+            "leagues" to leagues
+        )
+    }
 }
