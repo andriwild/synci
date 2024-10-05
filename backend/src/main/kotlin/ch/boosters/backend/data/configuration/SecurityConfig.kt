37639905bd728eecadfee36c905558a9c5cc95d1
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-05 16:29:07 +0200

    update cors and csrf restrictions

--- backend/src/main/kotlin/ch/boosters/backend/data/configuration/SecurityConfig.kt
+++ backend/src/main/kotlin/ch/boosters/backend/data/configuration/SecurityConfig.kt
@@ -2,7 +2,7 @@ package ch.boosters.backend.data.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
@@ -16,9 +16,12 @@ class SecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .csrf { it.disable() } // TODO: remove for production
             .cors { it.configurationSource(corsConfigurationSource()) }
             .authorizeHttpRequests { requests ->
                 requests
+                    .requestMatchers(HttpMethod.POST, "/api/syncconfig/new").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/syncconfig/*").permitAll()
                     .requestMatchers("**").permitAll()
             }
         return http.build()
@@ -27,7 +30,7 @@ class SecurityConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://127.0.0.1:5173")
+        configuration.allowedOrigins = listOf("http://127.0.0.1:5173", "http://127.0.0.1:3000", "http://localhost:3000", "http://localhost:5173")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
