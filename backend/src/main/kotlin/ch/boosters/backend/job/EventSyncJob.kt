aab3cf2e0691b6f394cf1a1ae2464efd95f99bd7
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-15 20:25:39 +0200

    move clear table to event class

--- backend/src/main/kotlin/ch/boosters/backend/job/EventSyncJob.kt
+++ backend/src/main/kotlin/ch/boosters/backend/job/EventSyncJob.kt
@@ -1,4 +1,6 @@
+import ch.boosters.backend.data.event.EventService
 import ch.boosters.backend.sources.fotmob.FotMobService
+import ch.boosters.backend.sources.swissski.SwissSkiService
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,8 +11,16 @@ class EventSyncJob : Job {
 
     @Autowired
     lateinit var fotMobService: FotMobService
+    @Autowired
+    lateinit var swissSkiService: SwissSkiService
+    @Autowired
+    lateinit var eventService: EventService
 
     override fun execute(context: JobExecutionContext) {
-        fotMobService.updateLeagues()
+        val result = eventService.clearTables()
+        if (result) {
+            fotMobService.updateLeagues()
+            swissSkiService.updateRaces()
+        }
     }
 }
