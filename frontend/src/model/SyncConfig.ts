339ba344311c611cac69289d9c7b1f6c7c97d5bc
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-17 18:53:20 +0200

    add ski sports to form

--- frontend/src/model/SyncConfig.ts
+++ frontend/src/model/SyncConfig.ts
@@ -1,7 +1,9 @@
 import { Team } from "./Team.ts";
+import { Sport } from "./Sport.ts";
 
 export interface SyncConfig {
     id: string | null;
     name: string;
     teams: Team[];
+    sports: Sport[];
 }
