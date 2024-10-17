5085c8590979863a2ef38dd263a8bde12b89f785
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-17 18:42:02 +0200

    update event to make end date optional for all day events

--- backend/src/main/kotlin/ch/boosters/backend/data/event/model/Event.kt
+++ backend/src/main/kotlin/ch/boosters/backend/data/event/model/Event.kt
@@ -3,4 +3,4 @@ package ch.boosters.backend.data.event.model
 import java.time.LocalDateTime
 
 
-data class Event(val name: String, val id: String, val startsOn: LocalDateTime, val endsOn: LocalDateTime)
+data class Event(val name: String, val id: String, val startsOn: LocalDateTime, val endsOn: LocalDateTime?)
