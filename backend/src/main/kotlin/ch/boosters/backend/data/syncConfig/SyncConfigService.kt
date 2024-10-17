7106c30db992bd695fd5d492f9afb8801fc10e25
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-17 18:46:56 +0200

    add queries to fetch syncConfig entries of sports table

--- backend/src/main/kotlin/ch/boosters/backend/data/syncConfig/SyncConfigService.kt
+++ backend/src/main/kotlin/ch/boosters/backend/data/syncConfig/SyncConfigService.kt
@@ -1,5 +1,6 @@
 package ch.boosters.backend.data.syncConfig
 
+import ch.boosters.backend.data.syncConfigSports.SyncConfigSportsRepository
 import ch.boosters.backend.data.syncConfigTeam.SyncConfigTeamRepository
 import org.springframework.stereotype.Service
 import java.util.*
@@ -7,12 +8,14 @@ import java.util.*
 @Service
 class SyncConfigService(
     private val syncConfigRepository: SyncConfigRepository,
-    private val syncConfigTeamRepository: SyncConfigTeamRepository
+    private val syncConfigTeamRepository: SyncConfigTeamRepository,
+    private val syncConfigSportsRepository: SyncConfigSportsRepository
 ) {
 
     fun createSyncConfig(syncConfig: SyncConfig): UUID {
         val id = syncConfigRepository.createSyncConfig(syncConfig)
         syncConfigTeamRepository.addTeams(id, syncConfig.teams)
+        syncConfigSportsRepository.addSports(id, syncConfig.sports)
         return id
     }
 
@@ -27,6 +30,7 @@ class SyncConfigService(
     fun updateSyncConfig(id: UUID, syncConfig: SyncConfig) {
         syncConfigRepository.updateSyncConfig(id, syncConfig)
         syncConfigTeamRepository.updateTeams(id, syncConfig.teams)
+        syncConfigSportsRepository.updateSports(id, syncConfig.sports)
     }
 
     fun deleteSyncConfig(id: UUID): Int {
