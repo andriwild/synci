8e5aa6e333d137d1ce3fc2060ad5f97c11af5111
Elias Braem <elias.braem@software-brauerei.ch>
2025-03-08 10:06:57 +0100

    small ui fixes

--- frontend/src/pages/sport/CalendarComponent.tsx
+++ frontend/src/pages/sport/CalendarComponent.tsx
@@ -1,19 +1,21 @@
-import {Button, Flex, Typography} from "antd";
+import {Alert, Button, Flex} from "antd";
 
 export const CalendarComponent = () => {
     return (
         <Flex vertical style={{gap: 20, padding: "0 20px"}}>
 
             <h1>Meine Abos</h1>
-
             <Flex vertical style={{flexDirection: "column", gap: 20}}>
-            <Typography.Text>
-                Du bis noch nicht Angemeldet
-            </Typography.Text>
-            <Button type="primary">Anmelden</Button>
+                <Alert
+                    message="Du bis noch nicht angemeldet"
+                    description="Melde dich an, um deine Abos zu sehen"
+                    type="warning"
+                    showIcon
+                    action={
+                        <Button>Anmelden</Button>
+                    }
+                />
             </Flex>
-
-
         </Flex>
     );
 }
