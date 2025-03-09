f4ad138e5a03bc0c1607d75b782bcda6f2997f0f
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2025-03-09 20:52:08 +0100

    clean up security config

--- backend/src/main/kotlin/ch/boosters/backend/config/SecurityConfiguration.kt
+++ backend/src/main/kotlin/ch/boosters/backend/config/SecurityConfiguration.kt
@@ -1,41 +1,32 @@
 package ch.boosters.backend.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.convert.converter.Converter
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.jwt.JwtDecoders
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-class SecurityConfiguration(
-    @Value("\${spring.security.oauth2.client.provider.synci-keycloak.issuer-uri}")
-    private val issuerUri: String,
-) {
+class SecurityConfiguration(private val corsProperties: CorsProperties){
 
-    private val clientName: String = "synci-backend"
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { it.disable() }
+        http
+            .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .authorizeHttpRequests {
-                it.requestMatchers("/swissski/list").permitAll()
-                    .requestMatchers("/team/list").permitAll()
+                it
+                    .requestMatchers(HttpMethod.GET, "/sports").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/sports/**").permitAll()
                     .anyRequest().authenticated()
-            }
-            .oauth2ResourceServer {
-                it.jwt { jwt ->
-                    jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
-                    jwt.jwtAuthenticationConverter(customJwtAuthenticationConverter())
-                }
             }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -45,15 +36,17 @@ class SecurityConfiguration(
     }
 
     @Bean
-    fun customJwtAuthenticationConverter(): JwtAuthenticationConverter {
-        println("clientName: $clientName")
-        return JwtAuthenticationConverter().apply {
-            setJwtGrantedAuthoritiesConverter(customJwtGrantedAuthoritiesConverter())
-        }
-    }
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(
+            corsProperties.allowedOrigins
+        )
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
 
-    @Bean
-    fun customJwtGrantedAuthoritiesConverter(): Converter<Jwt, Collection<GrantedAuthority>> {
-        return CustomJwtGrantedAuthoritiesConverter(clientName)
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
