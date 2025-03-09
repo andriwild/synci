72d39f5ca841c5985e9f51dd9445d34518ee4491
Elias Braem <elias.braem@software-brauerei.ch>
2025-03-09 21:41:03 +0100

    hotfix sync config cards

--- frontend/src/pages/sport/SportConfigCard.tsx
+++ frontend/src/pages/sport/SportConfigCard.tsx
@@ -2,12 +2,16 @@ import {Sport} from "../../services/sport/entities/sport.ts";
 import {Button, Flex, Tag, theme, Typography} from "antd";
 import {IconCalendarWeek, IconTrash} from "@tabler/icons-react";
 import {syncConfigApi} from "../../services/syncConfig/syncConfigApi.ts";
-import {useSyncConfig} from "../../services/syncConfig/syncCofigSlice.ts";
+import {syncConfigActions, useSyncConfig} from "../../services/syncConfig/syncCofigSlice.ts";
+import {syncConfigDtoMapper} from "../../services/syncConfig/helpers/syncConfigHelper.ts";
+import {useDispatch} from "react-redux";
 
 export const SportConfigCard = ({sport}: { sport: Sport }) => {
     const [updateSyncConfig, updateSyncConfigStatus] = syncConfigApi.useUpdateMutation();
     const syncConfig = useSyncConfig();
     const token = theme.useToken().token;
+    const dispatch = useDispatch();
+
     return (
         <Flex
             gap={20}
@@ -18,7 +22,7 @@ export const SportConfigCard = ({sport}: { sport: Sport }) => {
             <IconCalendarWeek size={40} color={token.colorPrimary}/>
                 <Flex vertical gap={10}
                       align={"start"}>
-                    <Typography.Title level={4}
+                    <Typography.Title level={5}
                     style={{margin: "0"}}
                     >{sport.name}</Typography.Title>
                     <Tag color={token.colorPrimary}>Sportart</Tag>
@@ -27,9 +31,15 @@ export const SportConfigCard = ({sport}: { sport: Sport }) => {
             <Button
                 type={"default"}
                 icon={<IconTrash size={20}/>}
-                onClick={() => {
-
-
+                loading={updateSyncConfigStatus.isLoading}
+                onClick={async () => {
+                    if (!syncConfig || !syncConfig.id) {
+                        return;
+                    }
+                    const dto = syncConfigDtoMapper(syncConfig);
+                    dto.sports = dto.sports?.filter((s) => s !== sport.id);
+                    const newSyncConfig = await updateSyncConfig(dto);
+                    dispatch(syncConfigActions.setSyncConfig(newSyncConfig.data));
                 }
                 }
             />
