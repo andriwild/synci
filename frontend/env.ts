89ed99b9ab85b583c8f9a15f8d281d4bcfb9fe29
Francesco Lucia <francesco.lucia@students.fhnw.ch>
2025-03-09 15:44:27 +0100

    added env variable in frontend login

--- frontend/env.ts
+++ frontend/env.ts
@@ -1,3 +1,5 @@
 export const VITE_URL_PREFIX = import.meta.env.VITE_URL_PREFIX
 export const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST
 export const VITE_KEYCLOAK_HOST = import.meta.env.VITE_KEYCLOAK_HOST
+export const VITE_SECRET_KEYCLOAK = import.meta.env.VITE_SECRET_KEYCLOAK
+export const VITE_CLIENT_ID_KEYCLOAK = import.meta.env.VITE_CLIENT_ID_KEYCLOAK
