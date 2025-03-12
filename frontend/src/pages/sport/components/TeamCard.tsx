d2e8ab2482f0ac5d44266792686dfe156222ed7e
Elias Braem <elias.braem@software-brauerei.ch>
2025-03-12 11:18:40 +0100

    add button action

--- frontend/src/pages/sport/components/TeamCard.tsx
+++ frontend/src/pages/sport/components/TeamCard.tsx
@@ -1,9 +1,17 @@
 import {Button, Flex, theme, Typography} from "antd";
 import {Team} from "../../../services/team/entities/team.ts";
 import {IconCalendarPlus, IconUsersGroup} from "@tabler/icons-react";
+import {useDispatch} from "react-redux";
+import {syncConfigDtoMapper} from "../../../services/syncConfig/helpers/syncConfigHelper.ts";
+import {syncConfigActions, useSyncConfig} from "../../../services/syncConfig/syncCofigSlice.ts";
+import {syncConfigApi} from "../../../services/syncConfig/syncConfigApi.ts";
 
 export const TeamCard = ({ team } : { team: Team }) => {
     const token = theme.useToken().token;
+    const dispatch = useDispatch();
+    const syncConfig = useSyncConfig();
+    const [updateSyncConfig, updateSyncConfigStatus] = syncConfigApi.useUpdateMutation();
+
     return (
         <Flex
             vertical
@@ -19,7 +27,18 @@ export const TeamCard = ({ team } : { team: Team }) => {
         >
             <IconUsersGroup size={30}/>
             <Typography.Text>{team.name}</Typography.Text>
-            <Button icon={<IconCalendarPlus size={20}/>} type="primary">Team Abonnieren</Button>
+            <Button icon={<IconCalendarPlus size={20}/>}
+                    onClick={async () => {
+                        if (!syncConfig || !syncConfig.id) {
+                            return;
+                        }
+                        const dto = syncConfigDtoMapper(syncConfig);
+                        dto.teams = [...(dto.teams ?? []), {id: team.id, sourceId: team.sourceId}];
+                        const response = await updateSyncConfig(dto);
+                        dispatch(syncConfigActions.setSyncConfig(response.data));
+                    }}
+                    loading={updateSyncConfigStatus.isLoading}
+                    type="primary">Team Abonnieren</Button>
         </Flex>
     );
 }
