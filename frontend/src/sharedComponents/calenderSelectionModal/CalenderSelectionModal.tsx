2702b97bb3e86f70d6da5387c5a81468db6ec079
Elias Braem <elias.braem@software-brauerei.ch>
2025-08-05 20:33:38 +0200

    comment onclose for pip

--- frontend/src/sharedComponents/calenderSelectionModal/CalenderSelectionModal.tsx
+++ frontend/src/sharedComponents/calenderSelectionModal/CalenderSelectionModal.tsx
@@ -28,7 +28,7 @@ export const CalendarSelectionModal = ({url, buttonText, buttonType, buttonIcon}
             >{buttonText}</Button>
             <Modal
                 open={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
+                //onClose={() => setIsModalOpen(false)}
                 onCancel={() => setIsModalOpen(false)}
                 width={screen.md ? "50%" : "90%"}
             >
